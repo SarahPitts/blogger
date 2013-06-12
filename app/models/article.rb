a564209da8c178faa7245006ed35d5b2cdf1213b
Sarah Pitts <mccallsmedia@gmail.com>
2013-06-12 13:20:38 -0500

    adds tagging

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,11 @@
 class Article < ActiveRecord::Base
   attr_accessible :title, :body
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+def tag_list
+  tags.join(", ")
+end
+
 end
